36be5ca2306123aa81a94a4b2ddd8b8b869f9a3d
para-commando <anirudh.nayak.172k@gmail.com>
2024-07-18 23:38:48 +0530

    dev and fixes 27

--- src/test/java/com/JournalOne/JournalOne/service/RedisTests.java
+++ src/test/java/com/JournalOne/JournalOne/service/RedisTests.java
@@ -1,27 +1,31 @@
 package com.JournalOne.JournalOne.service;
 
+
+import com.JournalOne.JournalOne.config.AppCache;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 public class RedisTests {
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Autowired
     private TasksService tasksService;
 
+
     @Test
-    void testSendMail()
-    {
-       // redisTemplate.opsForValue().set("email","paracomando.one@gmail.com");
+    void testSendMail() {
+        // redisTemplate.opsForValue().set("email","paracomando.one@gmail.com");
 
 //        Object email = redisTemplate.opsForValue().get("email");
 //        Object salary = redisTemplate.opsForValue().get("salaryPerMonth");
-        tasksService.getTasksList2();
+        assertNotNull(tasksService.getTasksList2());
     }
 
 }
